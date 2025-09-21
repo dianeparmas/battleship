4d893a5646116cfcfceb689398a5c37344ebe28b
Diane Parmas <diane.parmas@gmail.com>
2025-09-21 15:25:00 +0300

    updates tabbing

--- src/constants/shipImages.ts
+++ src/constants/shipImages.ts
@@ -1,8 +1,8 @@
 const SHIP_IMAGES = {
-    2: "ship_2",
-    3: "ship_3",
-    4: "ship_4",
-    5: "ship_5",
-}
+  2: "ship_2",
+  3: "ship_3",
+  4: "ship_4",
+  5: "ship_5",
+};
 
 export default SHIP_IMAGES;
