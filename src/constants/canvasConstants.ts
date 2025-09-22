f0552f92969b55addeadcb9b480977d31cbb79f7
Diane Parmas <diane.parmas@gmail.com>
2025-09-22 14:29:29 +0300

    updates constants

--- src/constants/canvasConstants.ts
+++ src/constants/canvasConstants.ts
@@ -2,7 +2,9 @@ export const CANVAS_SIZE = {
   WIDTH: 500,
   HEIGHT: 500,
   WIDTH_GRID: 550,
-  HEIGHT_GRID: 550
+  HEIGHT_GRID: 550,
 };
 
 export const GRID_CELL_SIZE = 50;
+
+export const SHIP_SIZES = [5, 4, 3, 3, 2];
