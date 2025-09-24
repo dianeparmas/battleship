58038d2b11612cbe9b5b6ad4bceb9c42f7536611
Diane Parmas <diane.parmas@gmail.com>
2025-09-24 12:25:18 +0300

    updates ShipPlacementCanvas types

--- src/types/ShipPlacementCanvas.types.ts
+++ src/types/ShipPlacementCanvas.types.ts
@@ -1,11 +1,8 @@
-import { Dispatch, SetStateAction } from "react";
-
 import { Ship } from "./battleship.types";
 
 export interface ShipPlacementCanvasProps {
   id: string;
   className?: string;
-  setShips: Dispatch<SetStateAction<Ship[]>>;
   handleBeginGame: (playerShips: Ship[]) => void;
   isGameTime: boolean;
 }
