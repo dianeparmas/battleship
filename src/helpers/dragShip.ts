dab6c3fec6b60bb9f0c65142c02fbf6dad985362
Diane Parmas <diane.parmas@gmail.com>
2025-09-27 12:47:07 +0300

    adds dragShip helper

---
+++ src/helpers/dragShip.ts
@@ -0,0 +1,69 @@
+import { dragShipParams } from "../types/dragShip.types";
+
+import { getMouseCoordinates } from "../utils/canvasUtils";
+
+import { drawRectangle } from "../drawing/drawing";
+
+export const dragShip = ({
+  event,
+  ctx,
+  shipPositions,
+  currentlyActiveShip,
+  canvasRef,
+  mouseOffsetRef,
+  movedShips,
+  movedShipRef,
+}: dragShipParams) => {
+  console.log(
+    `shipPositions: `,
+    shipPositions,
+    `currentlyActiveShip: `,
+    currentlyActiveShip,
+    `canvasRef: ${canvasRef}`,
+    canvasRef,
+    `mouseOffsetRef: ${mouseOffsetRef}`,
+    mouseOffsetRef,
+    `movedShips: ${movedShips}`,
+    movedShips,
+    `movedShipRef: ${movedShipRef}`,
+    movedShipRef,
+  );
+
+  if (ctx) {
+    const shipToMove = shipPositions.find(
+      (ship) =>
+        ship.x === currentlyActiveShip.current.x &&
+        ship.y === currentlyActiveShip.current.y,
+    );
+    const latestShip = shipToMove || {};
+
+    const { x, y } = getMouseCoordinates(event, canvasRef);
+    const newX = x - mouseOffsetRef.current.x;
+    const newY = y - mouseOffsetRef.current.y;
+    console.log("mouseoffcet x, y", x, y);
+    console.log("mouseoffcet newX newY", newX, newY);
+
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.fillStyle = "red";
+    ctx.fillRect(newX, newY, latestShip.width, latestShip.height);
+    latestShip.x = newX;
+    latestShip.y = newY;
+    console.log("%c New Rectangle Position:", "color: green", newX, newY);
+
+    // Redraw all completed rectangles
+    shipPositions.forEach((rect) => drawRectangle(ctx, rect));
+
+    movedShips.current.push({
+      x: newX,
+      y: newY,
+      width: latestShip.width,
+      height: latestShip.height,
+    });
+    movedShipRef.current = {
+      x: newX,
+      y: newY,
+      width: latestShip.width,
+      height: latestShip.height,
+    };
+  }
+};
