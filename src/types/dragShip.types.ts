45d8dae7b904f0b323416de5412995dcac58201d
Diane Parmas <diane.parmas@gmail.com>
2025-10-03 19:15:02 +0300

    updates dragShip types

--- src/types/dragShip.types.ts
+++ src/types/dragShip.types.ts
@@ -5,20 +5,24 @@ export interface dragShipParams {
   ctx: CanvasRenderingContext2D;
   shipPositions: Ship[];
   currentlyActiveShip: {
-    current: Ship
+    current: Ship;
   };
-  canvasRef: HTMLCanvasElement;
+  canvasRef: {
+    current: HTMLCanvasElement | null;
+  };
   mouseOffsetRef: {
     current: {
       x: number;
       y: number;
     };
-  }; 
+  };
   movedShips: {
     current: {
       x: number;
       y: number;
-    };
+      width: number;
+      height: number;
+    }[];
   };
   movedShipRef: {
     current: {
