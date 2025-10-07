b94fbb2918c23253519138a72ad667e81c16d227
Diane Parmas <diane.parmas@gmail.com>
2025-10-07 20:14:50 +0300

    attaches Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { ThemeProvider } from "./contexts/ThemeContext";
+
 import Game from "./components/Game/Game";
 
 import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
-      <h1>Battleship</h1>
-      <div className={styles.gameContainer}>
-        <Game />
-      </div>
-    </>
+    <ThemeProvider>
+      <>
+        <h1>Battleship</h1>
+        <div className={styles.gameContainer}>
+          <Game />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
